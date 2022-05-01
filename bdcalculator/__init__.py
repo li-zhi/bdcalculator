67d3819a696a28e90864b2cead88072f0ac1a48a
Zhi Li <zli@netflix.com>
2022-05-01 16:33:52 -0700

    Version 0.1.2

--- bdcalculator/__init__.py
+++ bdcalculator/__init__.py
@@ -3,7 +3,7 @@ from bdcalculator.bdcalculator import BDrateCalculator
 __copyright__ = "Copyright 2022, Zhi Li"
 __license__ = "MIT"
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 
 
 __all__ = ['BDrateCalculator']
