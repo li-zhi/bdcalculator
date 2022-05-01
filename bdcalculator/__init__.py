7f702a447c128ddcdd0d14f8ca5623b999336550
Zhi Li <zli@netflix.com>
2022-05-01 15:01:13 -0700

    set up local repo, copy code over, and have the first successful test.

---
+++ bdcalculator/__init__.py
@@ -0,0 +1,7 @@
+from bdcalculator.bdcalculator import BDrateCalculator
+
+__copyright__ = "Copyright 2022, Zhi Li"
+__license__ = "MIT"
+
+
+__all__ = ['BDrateCalculator']
---
+++ test/__init__.py
@@ -0,0 +1,2 @@
+__copyright__ = "Copyright 2022, Zhi Li"
+__license__ = "MIT"
